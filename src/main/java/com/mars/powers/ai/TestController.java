92051881515c085b2fd2a8418d928ba7bf1d5ee7
Mars <zm03322161@qq.com>
2023-02-19 11:23:10 +0800

    init

--- src/main/java/com/mars/powers/ai/TestController.java
+++ src/main/java/com/mars/powers/ai/TestController.java
@@ -12,6 +12,6 @@ public class TestController {
 
     @GetMapping(value = "/hello", name = "返回Hello World")
     public String hello(@RequestParam(value = "name", required = false) String name) {
-        return String.format("Hello %s! ", name == null ? "World" : name);
+        return String.format("Hello2 %s! ", name == null ? "World" : name);
     }
 }
